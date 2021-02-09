78902052fcb6dcc317358b8ac73dd555407260ef
AndrosVAV <andrevereshagin@gmail.com>
2021-02-09 18:31:05 +0400

    Цикл в цикле курс Е Попова

--- script.js
+++ script.js
@@ -201,3 +201,27 @@ switch(x){
 }
 
 */
+
+
+
+
+
+//Цикл в цикле     Курс Е.Попова - Javascript
+
+/*
+var students = ["Илья","Greg","Алина"];
+
+for(var i = 0;i < students.length;i++){
+    for(var j = 0;j < students[i].length;j++){
+        if(students[i].charCodeAt(j) < 1040 || students[i].charCodeAt(j) > 1103)
+        document.write("Найден символ кот не попадает в диапазон русских букв.Это символ: " + 
+        students[i].charAt(j) + " в элементе " + students[i] + "</br>")
+    }
+
+}
+
+
+*/
+
+
+
